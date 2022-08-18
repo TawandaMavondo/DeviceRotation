f38a049c904e3d809a7330b8b8a432972340a9a8
tawandamavondo <tawandagdmavondo@gmail.com>
2022-08-18 23:32:47 +0200

    .

--- main.js
+++ main.js
@@ -3,14 +3,41 @@ let gyroscope = new Gyroscope({ frequency: 60 });
 let x = document.querySelector(".x");
 let y = document.querySelector(".y");
 let z = document.querySelector(".z");
+let values = []
+rotated = 0.0
+start = false;
+startTime = 0
 
+function radians_to_degrees(radians) {
+    return radians * (180 / Math.PI);
+}
 gyroscope.addEventListener('reading', (e) => {
-    console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-    x.textContent = gyroscope.x;
-    y.textContent = gyroscope.y;
-    z.textContent = gyroscope.z;
-    console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-    console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+    x.textContent = "X: " + gyroscope.x;
+    y.textContent = "Y: " + gyroscope.y;
+    z.textContent = "Z: " + gyroscope.z;
+    if (start) {
+        let data = {
+            x: radians_to_degrees(gyroscope.x),
+            t: Math.floor(Date.now() / 1000) - startTime
+        }
+        values.push(data)
+    }
+
 });
+
+let btn = document.querySelector(".btn");
+let btn2 = document.querySelector(".btn2");
+
+btn.addEventListener("click", () => { start = true; startTime = Math.floor(Date.now() / 1000); console.log(startTime) })
+let rot = document.querySelector("rot");
+
+function calculateRotated() {
+    values.map(v => {
+        if (values < 0) return;
+        d = v.x * v.t
+        rotated += d;
+        rot.textContent = "Rotated: " + rotated
+    })
+}
 
 gyroscope.start();
